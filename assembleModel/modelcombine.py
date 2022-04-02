8973b736a3691bd83d39d5e7fbfa5bb175e1ce3e
neelkantnewra <newra008@gmail.com>
2022-04-02 18:49:15 +0530

    Comment added

--- assembleModel/modelcombine.py
+++ assembleModel/modelcombine.py
@@ -2,11 +2,24 @@
 '''
 @author neelkant newra
 date 02.04.2022
+
+Program to combine the model prediction
 '''
 import pickle
 import sklearn
 
 def combineModel(models:list,element):
+    '''
+    parameter
+    ---------
+    models: list of model prediction you want to combine
+    element: scalar vector of the input feeded in the model
+
+    return
+    ------
+    prediction: combine prediction
+    prediction_prob: combine prediction preobability
+    '''
     prediction = 0
     prediction_prob = 0
     for model in models:
